71a3c461db867477aff97da1fd2118d040d223b4
morsko1 <morsko1@yandex.ru>
2020-01-12 01:02:39 +0300

    added config

--- client/src/common/urls.js
+++ client/src/common/urls.js
@@ -1,3 +1,4 @@
+import config from '../../../config.js';
 const IS_PROD = process.env.NODE_ENV === 'production';
 
-export const baseUrl = IS_PROD ? process.env.API_URL : '';
+export const baseUrl = IS_PROD ? config.API_URL : '';
